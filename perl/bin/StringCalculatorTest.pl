e3ab69c25a08944380539edb38bc0c85dcb46b6c
custompro98 <mitchjoa@gmail.com>
2024-04-02 11:59:12 -0400

    chore: Use Test::More with done_testing()

--- perl/bin/StringCalculatorTest.pl
+++ perl/bin/StringCalculatorTest.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+package StringCalculatorTest;
+
 use strict;
 use warnings;
 
@@ -8,8 +10,8 @@ use Cwd  qw(abs_path);
 use lib dirname(dirname abs_path $0) . '/lib';
 
 use StringCalculator qw(add);
-# HINT: update me every time you add a test to avoid
-# '# Looks like you planned 1 test but ran n.'
-use Test::Simple tests => 1;
+use Test::More;
 
 ok( 1 eq 0, 'true is true' );
+
+done_testing();
